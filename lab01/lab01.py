53d56eda8791c218bf31976204dca62b7510620b
pjatgithub <pjatgithub@users.noreply.github.com>
2018-02-17 20:25:48 +0800

    Lab 01

---
+++ lab01/lab01.py
@@ -0,0 +1,85 @@
+"""Lab 1: Expressions and Control Structures"""
+
+# Coding Practice
+
+def repeated(f, n, x):
+    """Returns the result of composing f n times on x.
+
+    >>> def square(x):
+    ...     return x * x
+    ...
+    >>> repeated(square, 2, 3)  # square(square(3)), or 3 ** 4
+    81
+    >>> repeated(square, 1, 4)  # square(4)
+    16
+    >>> repeated(square, 6, 2)  # big number
+    18446744073709551616
+    >>> def opposite(b):
+    ...     return not b
+    ...
+    >>> repeated(opposite, 4, True)
+    True
+    >>> repeated(opposite, 5, True)
+    False
+    >>> repeated(opposite, 631, 1)
+    False
+    >>> repeated(opposite, 3, 0)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    count = 0
+
+    while count < n:
+        x = f(x)
+        count = count + 1
+    
+    return x
+def sum_digits(n):
+    """Sum all the digits of n.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    """
+    "*** YOUR CODE HERE ***"
+    total = 0
+    divider = 10
+
+    while n > 0:
+        quotient = n // divider
+        remainder = n % divider
+        total = total + remainder
+        n = quotient
+
+    return total
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    ret = False
+    target = 88
+
+    while n >= target:
+        last_two = n % 100
+
+        if last_two == target:
+            ret = True
+            break
+
+        n = n // 10
+
+    return ret
