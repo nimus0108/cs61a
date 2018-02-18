c5e9e7a716db86d058ca34f63ed62f027aca17bd
pjatgithub <pjatgithub@users.noreply.github.com>
2018-02-18 11:26:16 +0800

    Implement Problem 7

--- hog/hog.py
+++ hog/hog.py
@@ -214,6 +214,27 @@ def announce_highest(who, previous_high=0, previous_score=0):
     assert who == 0 or who == 1, 'The who argument should indicate a player.'
     # BEGIN PROBLEM 7
     "*** YOUR CODE HERE ***"
+    def say(score0, score1):
+        if who == 0:
+            current_score = score0
+        else:
+            current_score = score1
+
+        score_diff = current_score - previous_score
+
+        if score_diff > previous_high:
+            if score_diff == 1:
+                point = "point!"
+            else:
+                point = "points!"
+            print(score_diff, point, "That's the biggest gain yet for Player",
+                who)
+            
+            return announce_highest(who, score_diff, current_score)
+
+        return announce_highest(who, previous_high, current_score)
+
+    return say
     # END PROBLEM 7
 
 
