a4b77a761c37ccdbee55b4a625d9f5aeab3d1ec3
pjatgithub <pjatgithub@users.noreply.github.com>
2018-02-26 15:58:31 +0800

    Implement Question 2 of HW 03

--- hw03/hw03.py
+++ hw03/hw03.py
@@ -49,7 +49,12 @@ def summation(n, term):
     True
     """
     assert n >= 1
-    "*** YOUR CODE HERE ***"
+
+    if n == 1:
+        return term(n)
+    else:
+        return term(n) + summation(n - 1, term)
+
 
 def square(x):
     return x * x
