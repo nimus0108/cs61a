463c4d08a2e27fec5cee3df749e02f4b576f10a3
pjatgithub <pjatgithub@users.noreply.github.com>
2018-04-22 16:29:05 +0800

    Implement Question 10 of Lab 09

--- lab09/lab09_extra.scm
+++ lab09/lab09_extra.scm
@@ -83,7 +83,13 @@
 
 ; Q10
 (define (substitute s old new)
-  'YOUR-CODE-HERE
+  (cond
+    ((null? s) s)
+    ((pair? (car s)) (cons(substitute (car s) old new) 
+                       (substitute (cdr s) old new)))
+    ((eq? (car s) old) (cons new (substitute (cdr s) old new)))
+    (else (cons (car s) (substitute (cdr s) old new)))
+  )
 )
 
 ; Q11
