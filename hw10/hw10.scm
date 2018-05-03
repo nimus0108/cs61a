f9d414dba06a87c844377afd37583873ad42351a
pjatgithub <pjatgithub@users.noreply.github.com>
2018-05-04 01:42:00 +0800

    Implement Question 02 of HW 10

--- hw10/hw10.scm
+++ hw10/hw10.scm
@@ -6,7 +6,10 @@
 )
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 0)
+      start
+      (accumulate combiner (combiner start (term n)) (- n 1) term)
+  )
 )
 
 (define-macro (list-of expr for var in seq if filter-fn)
