e07dfa28aa460e601bbde05acbb53959085c0528
pjatgithub <pjatgithub@users.noreply.github.com>
2018-05-04 18:22:57 +0800

    Implement Question 06 of Project Scheme

--- scheme/scheme.py
+++ scheme/scheme.py
@@ -225,6 +225,9 @@ def do_define_form(expressions, env):
         check_form(expressions, 2, 2)
         # BEGIN PROBLEM 6
         "*** YOUR CODE HERE ***"
+        env.define(target, scheme_eval(expressions.second.first, env))
+
+        return target
         # END PROBLEM 6
     elif isinstance(target, Pair) and scheme_symbolp(target.first):
         # BEGIN PROBLEM 10
