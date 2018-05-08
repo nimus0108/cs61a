09cec4c041ee83898d5a01dc16327183e11566e2
pjatgithub <pjatgithub@users.noreply.github.com>
2018-05-08 12:39:56 +0800

    Implement Question 07 of LAB 11

--- lab11/lab11_extra.py
+++ lab11/lab11_extra.py
@@ -77,6 +77,16 @@ def merge(s0, s1):
     i0, i1 = iter(s0), iter(s1)
     e0, e1 = next(i0, None), next(i1, None)
     "*** YOUR CODE HERE ***"
+    while e0 != None or e1 != None:
+        if e0 == None or e1 < e0:
+            yield e1
+            e1 = next(i1, None)
+        elif e1 == None or e0 < e1:
+            yield e0
+            e0 = next(i0, None)
+        else:
+            yield e0
+            e0, e1 = next(i0, None), next(i1, None)
 
 # Q8
 def remainders_generator(m):
