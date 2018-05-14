5eae1b326e16d69731895edd193f58f7342ec194
pjatgithub <pjatgithub@users.noreply.github.com>
2018-05-14 09:47:58 +0800

    Implement Question 01 of HW 12

--- hw12/hw12.sql
+++ hw12/hw12.sql
@@ -29,7 +29,8 @@ CREATE TABLE sizes AS
 
 -- The size of each dog
 CREATE TABLE size_of_dogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT dogs.name, sizes.size FROM dogs, sizes
+    WHERE dogs.height > sizes.min AND dogs.height <= sizes.max;
 
 -- All dogs with parents ordered by decreasing height of their parent
 CREATE TABLE by_height AS
